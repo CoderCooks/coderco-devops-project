8e283c3bc44f05a7605cf4d651982d25c5e3c794
ahmed-osman <ahmed_osman04@hotmail.com>
2025-04-10 21:12:32 +0100

    feat: add Route 53 module for DNS and custom domain

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -1,7 +1,5 @@
 //Resource blocks for the Route53 components goes here
-resource "aws_route53_zone" "this" {
-  name = var.domain_name
-}
+
 
 data "aws_route53_zone" "existing" {
   name = "cokercooks.com"
