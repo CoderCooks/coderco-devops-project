f148a42ac7e82f0554e875973dec0d6e8a259cdc
ahmed-osman <ahmed_osman04@hotmail.com>
2025-04-10 20:43:15 +0100

    feat: add Route 53 module for DNS and custom domain

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -1,1 +1,48 @@
 //Resource blocks for the Route53 components goes here
+resource "aws_route53_zone" "this" {
+  name = var.domain_name
+}
+
+data "aws_route53_zone" "existing" {
+  name = "cokercooks.com"
+}
+
+resource "aws_route53_record" "app_alias_record" {
+  count   = var.record_type == "A" ? 1 : 0
+  zone_id = data.aws_route53_zone.existing.zone_id
+  name    = var.domain_name
+  type    = var.record_type
+
+  alias {
+    name                   = var.alb_dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = true
+  }
+
+  depends_on = [aws_route53_zone.this]
+}
+
+resource "aws_route53_record" "app_cname_record" {
+  count   = var.record_type == "CNAME" ? 1 : 0
+  zone_id = aws_route53_zone.this.zone_id
+  name    = var.domain_name
+  type    = "CNAME"
+  ttl     = 300
+  records = [var.alb_dns_name]
+
+  depends_on = [aws_route53_zone.this]
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.94.1"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "default"
+}
--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -1,1 +1,28 @@
 //Variables for the Route53 module
+variable "domain_name" {
+  description = "The base domain name for the hosted zone"
+  type        = string
+  default     = "cokercooks.com"  # domain
+}
+
+variable "alb_dns_name" {
+  description = "The DNS name of the Application Load Balancer"
+  type        = string
+  default     = "null" 
+}
+
+variable "record_type" {
+  description = "The type of DNS record to create (A or CNAME)"
+  type        = string
+  default     = "A"  
+  validation {
+    condition     = contains(["A", "CNAME"], var.record_type)
+    error_message = "The record_type must be either 'A' or 'CNAME'"
+  }
+}
+
+variable "alb_zone_id" {
+  description = "The hosted zone ID for the ALB"
+  type        = string
+  default     = "ZHURV8PSTC4K8"
+}
